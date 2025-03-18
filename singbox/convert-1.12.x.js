e7be13c5c10557aa7724d89acddcafc9944b1970
ralopa673 <154424113+ralopa673@users.noreply.github.com>
2025-03-18 15:41:20 +0800

    Update convert-1.12.x.js

--- singbox/convert-1.12.x.js
+++ singbox/convert-1.12.x.js
@@ -1,3 +1,4 @@
+//https://raw.githubusercontent.com/xishang0128/sub-store-template/refs/heads/main/1.12.x/sing-box.js
 const { type, name } = $arguments
 const compatible_outbound = {
   tag: 'COMPATIBLE',
@@ -19,20 +20,23 @@ config.outbounds.map(i => {
   if (['all', 'all-auto'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies))
   }
-  if (['hk', 'hk-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /港|hk|hongkong|kong kong|/i))
+  if (['HK', 'HK-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /港|hk|hong|🇭🇰/i))
   }
-  if (['tw', 'tw-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /台|tw|taiwan|/i))
+  if (['TW', 'TW-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /台|tw|taiwan|🇹🇼|🇨🇳/i))
   }
-  if (['jp', 'jp-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /日本|jp|japan|/i))
+  if (['JP', 'JP-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i))
   }
-  if (['sg', 'sg-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^(?!.*(?:us)).*(新|sg|singapore|)/i))
+  if (['SG', 'SG-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^(?!.*(?:us)).*(新|sg|singapore|🇸🇬)/i))
   }
-  if (['us', 'us-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|/i))
+  if (['US', 'US-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i))
+  }
+  if (['EU', 'EU-auto'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /德|de|german|🇩🇪|荷|nl|nether|🇳🇱|英|gb|uk|kingdom|🇬🇧|意|it|🇮🇹|匈牙利|hungary|🇭🇺|芬兰|fi|🇫🇮|爱尔兰|ie|🇮🇪|欧|eu|🇪🇺/i))
   }
 })
 
@@ -51,4 +55,3 @@ $content = JSON.stringify(config, null, 2)
 function getTags(proxies, regex) {
   return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
 }
-
